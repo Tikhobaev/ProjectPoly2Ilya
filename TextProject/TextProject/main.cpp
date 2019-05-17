410edb42d3de342c6a4d312fd5911e43a39ad33f
Ilya Solovyov <ilyasolovyov52@yandex.ru>
2019-05-17 12:01:20 +0300

    First commit

---
+++ TextProject/TextProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TextProject
+//
+//  Created by Илья Соловьёв on 17/05/2019.
+//  Copyright © 2019 Илья Соловьёв. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

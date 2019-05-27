477726a7aabdcc53377db36375b049352fc1e74b
Polina Demochkina <lollipoli@live.com>
2019-05-27 13:52:04 +0400

    Added graphics

--- towerOfHanoi/towerOfHanoi.cpp
+++ towerOfHanoi/towerOfHanoi.cpp
@@ -4,11 +4,14 @@
 #include <cstdio>
 #include <conio.h>
 #include "stack.h"
+#include <cstring>
 #define NUMBER_OF_DISKS 3
 
 void menu();
 void rules();
 void play();
+void display(node*, node*, node*);
+void center_print(const char *, int );
 
 void main()
 {
@@ -89,18 +92,18 @@ void play()
 	result = push(result, 2);
 	result = push(result, 1);
 
+	int error = 0, error2 = 0;
+
 	while(!compare(result, destination))
 	{
 		system("cls");
-		printf("peg 1:  ");
-		print(source);
-		printf("\n");
-		printf("peg 2:  ");
-		print(temporary);
-		printf("\n");
-		printf("peg 1:  ");
-		print(destination);
-		printf("\n");
+		display(source, temporary, destination);
+		if(error==1)
+			printf("This move is not allowed, please try again.\n\n");
+		else if (error2 == 1)
+			printf("Wrong input, try again.\n\n");
+		error = 0;
+		error2 = 0;
 		printf("Move from: ");
 		scanf("%d", &from);
 		printf("\nMove to: ");
@@ -111,7 +114,7 @@ void play()
 		case 1:
 			if (isEmpty(source))
 			{
-				printf("This move is not allowed, please try again");
+				error = 1;
 				break;
 			}
 			if (to == 2)
@@ -122,7 +125,7 @@ void play()
 					source = pop(source);
 					break;
 				}
-				printf("This move is not allowed, please try again");
+				error = 1;
 				break;
 			}
 			else if (to == 3)
@@ -133,18 +136,18 @@ void play()
 					source = pop(source);
 					break;
 				}
-				printf("This move is not allowed, please try again");
+				error = 1;
 				break;
 			}
 			else
 			{
-				printf("Wrong input, try again");
+				error2 = 1;
 				break;
 			}
 		case 2:
 			if (isEmpty(temporary))
 			{
-				printf("This move is not allowed, please try again");
+				error = 1;
 				break;
 			}
 			if (to == 1)
@@ -155,7 +158,7 @@ void play()
 					temporary = pop(temporary);
 					break;
 				}
-				printf("This move is not allowed, please try again");
+				error = 1;
 				break;
 			}
 			else if (to == 3)
@@ -166,18 +169,18 @@ void play()
 					temporary = pop(temporary);
 					break;
 				}
-				printf("This move is not allowed, please try again");
+				error = 1;
 				break;
 			}
 			else
 			{
-				printf("Wrong input, try again");
+				error2 = 1;
 				break;
 			}
 		case 3:
 			if (isEmpty(destination))
 			{
-				printf("This move is not allowed, please try again");
+				error = 1;
 				break;
 			}
 			if (to == 2)
@@ -188,7 +191,7 @@ void play()
 					destination = pop(destination);
 					break;
 				}
-				printf("This move is not allowed, please try again");
+				error = 1;
 				break;
 			}
 			else if (to == 1)
@@ -199,20 +202,157 @@ void play()
 					destination = pop(destination);
 					break;
 				}
-				printf("This move is not allowed, please try again");
+				error = 1;
 				break;
 			}
 			else
 			{
-				printf("Wrong input, try again");
+				error2 = 1;
 				break;
 			}
 		default:
-			printf("wrong input, try again");
+			error2 = 1;
 			break;
 		}
 	}
+
 	printf("\nCongratulations, you won!\n\n");
 	system("pause");
 	menu();
+}
+
+void display(node* source, node* temporary, node* destination)
+{
+	const char* stack[3][3] = { {" "," "," "},{" "," "," "},{" "," "," "} };
+	node* peg1 = source;
+	node* peg2 = temporary;
+	node* peg3 = destination;
+
+	const int count1 = count(source);
+	const int count2 = count(temporary);
+	const int count3 = count(destination);
+	int r = 0, c = 0;
+
+	//draw peg1
+	if (count1 != 0)
+	{
+		for (int i = 0; i < 3 - count1; i++)
+		{
+			stack[r][c] = " ";
+			r++;
+		}
+		for (int i = 0; i < count1; i++)
+		{
+			if (peg1->value == 1)
+			{
+				stack[r][c] = "*";
+				r++;
+			}
+			else if (peg1->value == 2)
+			{
+				stack[r][c] = "***";
+				r++;
+			}
+			else if (peg1->value == 3)
+			{
+				stack[r][c] = "*****";
+				r++;
+			}
+			peg1 = peg1->pnext;
+		}
+	}
+
+	if (count2 != 0)
+	{
+		r = 0;
+		c = 1;
+		//draw peg2
+		for (int i = 0; i < 3 - count2; i++)
+		{
+			stack[r][c] = " ";
+			r++;
+		}
+		for (int i = 0; i < count2; i++)
+		{
+			if (peg2->value == 1)
+			{
+				stack[r][c] = "*";
+				r++;
+			}
+			else if (peg2->value == 2)
+			{
+				stack[r][c] = "***";
+				r++;
+			}
+			else if (peg2->value == 3)
+			{
+				stack[r][c] = "*****";
+				r++;
+			}
+			peg2 = peg2->pnext;
+		}
+	}
+
+	if (count3 != 0)
+	{
+		r = 0;
+		c = 2;
+		//draw peg3
+		for (int i = 0; i < 3 - count3; i++)
+		{
+			stack[r][c] = " ";
+			r++;
+		}
+		for (int i = 0; i < count3; i++)
+		{
+			if (peg3->value == 1)
+			{
+				stack[r][c] = "*";
+				r++;
+			}
+			else if (peg3->value == 2)
+			{
+				stack[r][c] = "***";
+				r++;
+			}
+			else if (peg3->value == 3)
+			{
+				stack[r][c] = "*****";
+				r++;
+			}
+			peg3 = peg3->pnext;
+		}
+	}
+
+	center_print(stack[0][0], 10);
+	center_print(stack[0][1], 10);
+	center_print(stack[0][2], 10);
+	printf("\n");
+	center_print(stack[1][0], 10);
+	center_print(stack[1][1], 10);
+	center_print(stack[1][2], 10);
+	printf("\n");
+	center_print(stack[2][0], 10);
+	center_print(stack[2][1], 10);
+	center_print(stack[2][2], 10);
+	printf("\n");
+	printf("================================\n");
+	center_print("1", 10);
+	center_print("2", 10);
+	center_print("3", 10);
+	printf("\n\n");
+}
+
+void center_print(const char *s, int width)
+{
+	const int length = strlen(s);
+	int i;
+	for (i = 0; i <= (width - length) / 2; i++) {
+		fputs(" ", stdout);
+	}
+	fputs(s, stdout);
+	i += length;
+	for (; i <= width; i++) {
+		fputs(" ", stdout);
+	}
 }
